ba26b1995ca25fb57040ae52c9be042702818d8f
Dragon Lore <tate.j.foster@gmail.com>
2022-08-10 08:51:03 -0600

    worked on questions in JS. Got it so that answers will now be on random buttons instead of the same location each time.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,15 @@ var buttonEl = document.querySelectorAll(".answer")
 var job = document.querySelector("h3")
 var correct = 0;
 var question = document.querySelectorAll("#question");
-var questionAll= ["Which of the following choices makes a variable into an object?", "Which shows the correct way of selecting a class in the related html document?",]
+var questionAll= ["Which of the following choices makes a variable into an object?", "Which shows the correct way of selecting a class in the related html document?", "What type of function allows you to itterate through an array?", "What type of Event Listener is used to start this quiz?"]
 var answersAll = {
-    answersOne: ["var example = []", "var example = {}", 'var example = ""', "var example = ()" ],
-    answersTwo: ['document.querySelectorAll("#class")', 'document.getElementByID("class")', 'document.querySelectorAll(".class")', 'document.querySelectorAll("class")' ]
+    answersOne: ["var example = [ ]", "var example = { }", 'var example = " "', "var example = ( )" ],
+    answersTwo: ['document.querySelectorAll("#class")', 'document.getElementByID("class")', 'document.querySelectorAll(".class")', 'document.querySelectorAll("class")' ],
+    answersThree: ["if() {} else {}", "switch() {}", "function() {}", "for() {}"],
+    answersFour: ["click", "mouseover", "keydown", "load"]
 }
+
+var timer = 30;
 var index = 0;
 console.log(answersAll.answersOne)
 
@@ -17,26 +21,44 @@ var answerQuestion = function (next) {
     question[0].textContent = questionAll[index]
 
     
-    switch (questionAll[index]) {
+    switch (question[0].textContent) {
 
         case questionAll[0]:
 
-            for (i = 0; i < answersAll.answersOne.length; i++) {
-            var randomAnswer = answersAll.answersOne[i]
-            buttonEl[i].textContent = randomAnswer }
+            for (i = 0; i < buttonEl.length; i++) {
+            r = Math.floor(answersAll.answersOne.length * Math.random())
+            buttonEl[i].textContent = answersAll.answersOne[r]
+            answersAll.answersOne.splice(r, 1)}
 
             break;
         
         case questionAll[1]:
 
-            for (i = 0; i < answersAll.answersTwo.length; i++) {
-                var randomAnswer = answersAll.answersTwo[i]
-                buttonEl[i].textContent = randomAnswer }
+            for (i = 0; i < buttonEl.length; i++) {
+                r = Math.floor(answersAll.answersTwo.length * Math.random())
+                buttonEl[i].textContent = answersAll.answersTwo[r]
+                answersAll.answersTwo.splice(r, 1)}
+            break;
 
+        case questionAll[2]:
+
+            for (i = 0; i < buttonEl.length; i++) {
+                r = Math.floor(answersAll.answersThree.length * Math.random())
+                buttonEl[i].textContent = answersAll.answersThree[r]
+                answersAll.answersThree.splice(r, 1)}
+            break;
+
+            case questionAll[3]:
+
+                for (i = 0; i < buttonEl.length; i++) {
+                    r = Math.floor(answersAll.answersFour.length * Math.random())
+                    buttonEl[i].textContent = answersAll.answersFour[r]
+                    answersAll.answersFour.splice(r, 1)}
+                break;
         }
     // console.log(randomAnswer)
     // }
-    if (index < questionAll.length) {
+    if (index < questionAll.length ) {
     index = index + next 
     } else {question[0].textContent = "That's all! You scored " + correct + " out of " + questionAll.length}
 }
@@ -49,11 +71,14 @@ var answerQuestion = function (next) {
 
 for (i = 0; i < buttonEl.length; i++) {
 buttonEl[i].addEventListener("click", function(event) {
-    if (event.target.textContent === "var example = {}" || event.target.textContent === 'document.querySelectorAll(".class")') {
+    if (event.target.textContent.includes("Quiz")){} else {
+    if (event.target.textContent === "var example = { }" || event.target.textContent === 'document.querySelectorAll(".class")' || event.target.textContent === "for() {}" || event.target.textContent === "click") {
         correct++;
-        job[0].textContent = "Correct!"
-        
+        job.textContent = "That's Correct!"
+        job.dataset.answer = "correct";
+
         console.log(correct)
-    }
+    } else {job.textContent = "Sorry that's wrong!";
+     job.dataset.answer = "incorrect";}}
     answerQuestion(1);
 })};
